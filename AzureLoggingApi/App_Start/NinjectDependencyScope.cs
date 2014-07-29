07d0a623066880701ece704820b404b68545b7b5
ppatarinski <paulpatarinski@gmail.com>
2014-07-29 00:53:30 -0500

    Adding NInject

---
+++ AzureLoggingApi/App_Start/NinjectDependencyScope.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AzureLoggingApi.App_Start
+{
+  public class NinjectDependencyScope
+  {
+  }
+}
