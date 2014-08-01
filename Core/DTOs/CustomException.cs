08f360cb74387b2c3b6ed8d5168f3a70635a963f
ppatarinski <paulpatarinski@gmail.com>
2014-08-01 18:25:02 -0500

    Adding AppVersion to the payload

--- Core/DTOs/CustomException.cs
+++ Core/DTOs/CustomException.cs
@@ -3,12 +3,12 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace Core.DTOs
 {
- 
   [BsonIgnoreExtraElements]
   public class CustomException
   {
     public string Message { get; set; }
     public string MethodName { get; set; }
     public DateTime ExceptionDate { get; set; }
+    public string AppVersion { get; set; }
   }
 }
