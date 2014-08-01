2d27cb23edf4f4b916dd5b4de7a7eab0e2351f2a
ppatarinski <paulpatarinski@gmail.com>
2014-07-31 22:34:48 -0500

    Adding a datatable to the home page

---
+++ Web/Scripts/home.js
@@ -0,0 +1,13 @@
+﻿  $(document).ready(function () {
+    $.getJSON("../api/exceptions", function (data) {
+
+      $('#table_id').DataTable({
+        data: data,
+        columns: [
+            { data: 'MethodName', width:'10%'},
+            { data: 'Message' , width :'80%'},
+            { data: 'ExceptionDate', width: '80%' },
+        ]
+      });
+    });
+  });
